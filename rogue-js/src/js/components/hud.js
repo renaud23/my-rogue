b71b6f773532e8a537946e698295e8ac757fb492
Renaud Genevois <renaud.genevois@insee.fr>
2018-03-14 14:59:07 +0100

    lose and win

--- rogue-js/src/js/components/hud.js
+++ rogue-js/src/js/components/hud.js
@@ -1,7 +1,8 @@
 import React from "react";
 export class JoueurHud extends React.Component {
   render() {
-    // for(int i=0;)
+    const { joueur } = this.props;
+    for (let i = 0; i < joueur.life; i++) {}
     return <div className="hud-joueur">HUD</div>;
   }
 }
